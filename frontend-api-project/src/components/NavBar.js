c171c408a71cf47f10c82ee6a097f491f9cc1b03
Andela Stevanovic <andjela.s166@gmail.com>
2022-06-02 23:39:55 -0400

    update NavBar

--- frontend-api-project/src/components/NavBar.js
+++ frontend-api-project/src/components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
       <ul>
           <li><NavLink to="/">Home</NavLink></li>
           <li><NavLink to="/destination">Destinations</NavLink></li>
-          <li><NavLink to="/attraction">Attractions</NavLink></li>
+          <li><NavLink to="/attraction">Find Attractions</NavLink></li>
       </ul>
     </div>
   )
