3aeaf2a992ef8e483bcdb4fb822c33c69863708f
Andela Stevanovic <andjela.s166@gmail.com>
2022-06-03 17:12:02 -0400

    update controlled form

--- frontend-api-project/src/components/NewAttraction.js
+++ frontend-api-project/src/components/NewAttraction.js
@@ -3,13 +3,17 @@ import React, { useState } from 'react'
 const NewAttraction = () => {
     const [ name, setName ] = useState("");
 
+    const handleChange = e => {
+        setName(e.target.value)
+    }
+
   return (
     <div>
       <h1>New Attraction</h1>
       <form>
           <div>
               <label>Attraction Name: </label>
-              <input type="text" value={ name } />
+              <input type="text" value={ name } onChange={ handleChange }/>
           </div>
         <br />
           <input type="submit" value="Create Attraction"/>
